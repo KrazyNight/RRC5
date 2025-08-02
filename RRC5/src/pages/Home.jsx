4f3a45ea2c9b017cd5287d0d29a7835fdf6a7608
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-08-02 09:34:11 -0700

    sole routers part 3

--- RRC5/src/pages/Home.jsx
+++ RRC5/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+
+///** 
 import { useEffect, useState } from "react";
 import axios from "axios";
 import User from "../components/User.jsx";
@@ -39,7 +41,7 @@ function Home() {
 }
 
 export default Home;
-
+//*/
 /**
 //Step one 
 //Hw: find a way to display data in frontend/page.
@@ -69,3 +71,54 @@ function Home() {
 export default Home;
 
  */
+/** 
+//Step one: solo iy too 
+
+//Hw: find a way to display data in frontend/page.
+import axios from "axios";
+import { useEffect, useState } from "react";
+import User from "../components/User.jsx";
+import { Link } from "react-router-dom";
+
+
+ 
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  async function fetchData() {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+
+
+
+
+
+  // "https://jsonplaceholder.typicode.com/users"
+        
+
+
+  return (
+    <>
+    <div>
+      {users.map((user) => (
+      <Link key={user.id} to={`/users/${user.id}`}>
+      <User 
+      id={user.id}
+      name={user.name}
+      username={user.username}
+      />
+      </Link>
+))};
+    </div>
+    </>
+  ) 
+}
+
+export default Home;
+*/
--- RRC5/src/pages/Users.jsx
+++ RRC5/src/pages/Users.jsx
@@ -1,3 +1,4 @@
+/** 
 //hw: no clue
 
 import { useParams } from "react-router-dom";
@@ -11,6 +12,43 @@ function Users() {
 
     return (
         <div>{ id }</div>
+    )
+}
+
+export default Users;
+//*/
+//hw: no clue
+
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+function Users() {
+    const { id } = useParams()
+    const [array, setArray] = useState({});
+
+    async function fetchId() {
+        
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+        setArray(data)
+    }
+
+    useEffect(() => {
+        fetchId();
+    }, [])
+
+
+
+
+    return (
+        <div>
+            <Link to='/'>Back to Home</Link>
+            <h1>{array.id}</h1>
+            <h1>{array.name}</h1>
+            <h1>{array.username}</h1>
+        </div>
     )
 }
 
